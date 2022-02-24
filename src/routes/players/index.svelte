66b1f0c7db0a868470358b9a51992ff5f432c94a
John Riordan <iam.john.riordan@gmail.com>
2022-02-23 20:27:00 -0800

    feat: player sorting

--- src/routes/players/index.svelte
+++ src/routes/players/index.svelte
@@ -1,19 +1,55 @@
 <script>
 	import PageHeader from '$lib/components/PageHeader.svelte';
+	import SortDirection from '$lib/components/SortDirection.svelte';
 	import { store } from '$lib/stores';
 
 	let search = '';
 	let list = [];
+	let sort = 'lp';
+	let desc = true;
 
 	$: players = $store.aggregate?.players || {};
 	$: list = Object.values(players)
-		.sort((a, b) => b.lp - a.lp)
-		.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()));
+		.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
+		.sort((a, b) => (desc ? b[sort] - a[sort] : a[sort] - b[sort]));
+
+	function setSort(col) {
+		if (sort !== col) sort = col;
+		else desc = !desc;
+	}
 </script>
 
 <PageHeader title="Players" />
 <div class="controls">
 	<input class="search" placeholder="Search Players" bind:value={search} />
+</div>
+<div class="sort">
+	<span class="nameSort">Name</span>
+	<span class="stat" on:click={() => setSort('lp')}>
+		LP
+		{#if sort === 'lp'}
+			<SortDirection class={desc ? 'desc' : 'asc'} />
+		{/if}
+	</span>
+	<span class="stat" on:click={() => setSort('kda')}>
+		KDA
+		{#if sort === 'kda'}
+			<SortDirection class={desc ? 'desc' : 'asc'} />
+		{/if}
+	</span>
+	<span class="stat" on:click={() => setSort('winrate')}>
+		Win-Rate
+		{#if sort === 'winrate'}
+			<SortDirection class={desc ? 'desc' : 'asc'} />
+		{/if}
+	</span>
+	<span class="stat"> W/L </span>
+	<span class="stat" on:click={() => setSort('cs')}>
+		Games
+		{#if sort === 'games'}
+			<SortDirection class={desc ? 'desc' : 'asc'} />
+		{/if}
+	</span>
 </div>
 <ul class="list">
 	{#each list as player}
@@ -61,7 +97,8 @@
 		gap: calc(var(--gap) / 3);
 	}
 
-	.list li a {
+	.list li a,
+	.sort {
 		--player-bg: var(--c2);
 		display: flex;
 		align-items: center;
@@ -81,6 +118,24 @@
 		font-weight: 800;
 		text-transform: uppercase;
 		letter-spacing: 2px;
+	}
+
+	.sort {
+		background: transparent;
+		font-weight: 700;
+		padding-bottom: 1rem;
+		user-select: none;
+	}
+	.sort > * {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.5rem;
+		cursor: pointer;
+	}
+	:global(.sort > * svg) {
+		width: 1rem;
+		height: 1rem;
 	}
 
 	.list .stat {
