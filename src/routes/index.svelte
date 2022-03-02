c894b839e6ef8c18850d42f98c00287a7e28f0c5
John Riordan <iam.john.riordan@gmail.com>
2022-03-01 21:31:15 -0800

    feat: update homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,5 +2,4 @@
 	export const prerender = true;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Champions Queue</h1>
