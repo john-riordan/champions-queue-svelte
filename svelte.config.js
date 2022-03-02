ed4132733f4cafef83d216ea703d94671ec75bdc
John Riordan <iam.john.riordan@gmail.com>
2022-03-01 21:48:11 -0800

    fix: revert adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess(),
 	kit: {
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
