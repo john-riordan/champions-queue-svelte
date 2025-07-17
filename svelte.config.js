718fcc5b748f2382d69054872e7da9683ee98178
John Riordan <iam.john.riordan@gmail.com>
2025-07-17 12:10:15 -0700

    chore: adapter vecel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-vercel';
+
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,17 +9,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		serviceWorker: {
-			register: true,
-			files: (filepath) => !/\.DS_Store/.test(filepath)
-		},
-		version: {
-			name: Date.now().toString(),
-			pollInterval: 0
-		}
+		adapter: adapter({ runtime: 'nodejs22.x' }),
+		serviceWorker: { register: true, files: (filepath) => !/\.DS_Store/.test(filepath) },
+		version: { name: Date.now().toString(), pollInterval: 0 }
 	}
 };
 
 export default config;
-
